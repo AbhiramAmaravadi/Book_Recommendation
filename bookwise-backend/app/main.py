f29737e8add3747540d31280c119f30a960b470c
Amaravadi <aamaravadi@wisc.edu>
2026-06-09 15:37:28 -0500

    fixed prodcution cors

--- bookwise-backend/app/main.py
+++ bookwise-backend/app/main.py
@@ -1,20 +1,24 @@
-from fastapi import FastAPI
-from sqlalchemy import text
-from app.routers.books import router as books_router
-from app.db import engine
+\from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import app.models   
+from sqlalchemy import text
+
+from app.db import engine
+from app.routers.books import router as books_router
 from app.routers.recommendations import router as recommendation_router
+import app.models
+
 app = FastAPI(
     title="BookWise AI",
     version="1.0.0"
 )
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
-        "http://localhost:8080"
+        "http://localhost:8080",
+        "https://book-recommendation-1-anse.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
@@ -26,12 +30,18 @@ app.include_router(
     prefix="/recommendations",
     tags=["Recommendations"]
 )
+
+app.include_router(
+    books_router,
+    prefix="/books",
+    tags=["Books"]
+)
+
 @app.get("/")
 def root():
     return {
         "message": "BookWise AI Backend Running"
     }
-
 
 @app.get("/db-test")
 def db_test():
@@ -41,8 +51,3 @@ def db_test():
     return {
         "database": "connected"
     }
-app.include_router(
-    books_router,
-    prefix="/books",
-    tags=["Books"]
-)
